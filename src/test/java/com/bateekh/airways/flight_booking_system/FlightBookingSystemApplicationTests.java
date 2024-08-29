0da0dc52ef9d2ea55c5241c5a49e86cb0d8d3bd2
Ahmad Abuwardeh <ahm.abuwardeh@gmail.com>
2024-08-29 20:48:56 +0300

    initial commit

---
+++ src/test/java/com/bateekh/airways/flight_booking_system/FlightBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bateekh.airways.flight_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
